1072fea638bab11536a6096de2254a9d5d87df79
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-01-31 17:25:34 +0100

    Setting up the US me

--- app/src/main/java/com/example/quickworkout/BMIActivity.kt
+++ app/src/main/java/com/example/quickworkout/BMIActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.google.android.material.textfield.TextInputLayout
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
+
+    val METRIC_UNITS_VIEW = "METRIC_UNIT_VIEW"
+    val US_UNITS_VIEW = "US_UNITS_VIEW"
+
+    var currentVisibleView: String = METRIC_UNITS_VIEW
+
     private lateinit var tvBMIValue: TextView
     private lateinit var tvBMIType: TextView
     private lateinit var tvBMIDescription: TextView
@@ -42,6 +49,45 @@ class BMIActivity : AppCompatActivity() {
                 Toast.makeText(this, " Please enter valid values", Toast.LENGTH_SHORT).show()
             }
         }
+        makeVisibleMetricUnitsView()
+
+        //set the units view based on the checked radio button
+        findViewById<RadioGroup>(R.id.rgUnits).setOnCheckedChangeListener { group, checkedId ->
+            if (checkedId == R.id.rbMetricUnits) {
+                makeVisibleMetricUnitsView()
+            } else {
+                makeVisibleUSUnitsView()
+            }
+        }
+    }
+
+    private fun makeVisibleMetricUnitsView() {
+        currentVisibleView = METRIC_UNITS_VIEW
+        findViewById<TextInputLayout>(R.id.tilMetricUnitWeight).visibility = View.VISIBLE
+        findViewById<TextInputLayout>(R.id.tilMetricUnitHeight).visibility = View.VISIBLE
+
+        findViewById<EditText>(R.id.etMetricUnitHeight).text.clear()
+        findViewById<EditText>(R.id.etMetricUnitWeight).text.clear()
+
+        findViewById<TextInputLayout>(R.id.tilUsUnitWeight).visibility = View.GONE
+        findViewById<LinearLayout>(R.id.llUsUnitsHeight).visibility = View.GONE
+
+        findViewById<LinearLayout>(R.id.llDiplayBMIResult).visibility = View.GONE
+    }
+
+    private fun makeVisibleUSUnitsView() {
+        currentVisibleView = US_UNITS_VIEW
+        findViewById<TextInputLayout>(R.id.tilMetricUnitWeight).visibility = View.VISIBLE
+        findViewById<TextInputLayout>(R.id.tilMetricUnitHeight).visibility = View.VISIBLE
+
+        findViewById<EditText>(R.id.etUsUnitWeight).text.clear()
+        findViewById<EditText>(R.id.etUsUnitHeightFeet).text.clear()
+        findViewById<EditText>(R.id.etUsUnitHeightInch).text.clear()
+
+        findViewById<TextInputLayout>(R.id.tilUsUnitWeight).visibility = View.GONE
+        findViewById<LinearLayout>(R.id.llUsUnitsHeight).visibility = View.GONE
+
+        findViewById<LinearLayout>(R.id.llDiplayBMIResult).visibility = View.GONE
     }
 
     private fun validateMetricUnits(): Boolean {
@@ -90,10 +136,8 @@ class BMIActivity : AppCompatActivity() {
             bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
         }
 
-        findViewById<TextView>(R.id.tvYourBMI).visibility = View.VISIBLE
-        findViewById<TextView>(R.id.tvBMIValue).visibility = View.VISIBLE
-        findViewById<TextView>(R.id.tvBMIType).visibility = View.VISIBLE
-        findViewById<TextView>(R.id.tvBMIDescription).visibility = View.VISIBLE
+        findViewById<LinearLayout>(R.id.llDiplayBMIResult).visibility = View.VISIBLE
+
 
         val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
 
